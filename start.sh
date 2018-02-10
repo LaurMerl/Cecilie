ee8f31dfaa9f624a42b4798e104c49f08c4daa6f
LaurMerl <l.merlo4@gmail.com>
2018-02-10 13:36:36 +0100

    Refactoring setup

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-sbt docker
 docker network create cecilie
 docker run -d -p 27017:27017 --name mongodb   --network cecilie mongo 
 docker run -d -p 8910:8910   --name phantomjs --network cecilie wernight/phantomjs phantomjs --webdriver=8910
